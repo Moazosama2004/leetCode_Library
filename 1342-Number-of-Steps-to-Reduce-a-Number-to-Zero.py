2e5c7a0aa1deaefacf060b6fdee74946de79bf37
Moaz osama <102158567+Moazosama2004@users.noreply.github.com>
2024-07-27 04:00:25 +0300

    [1342] [Time Beats: 99.90%] [Memory Beats: 96.27%] - LeetPush

--- 1342-Number-of-Steps-to-Reduce-a-Number-to-Zero.py
+++ 1342-Number-of-Steps-to-Reduce-a-Number-to-Zero.py
@@ -2,8 +2,8 @@ class Solution:
     def numberOfSteps(self, num: int) -> int:
         iter = 0
         while (num != 0):
-            if num % 2 == 0:
-                num = int(num / 2)
+            if num & 1 == 0:
+                num >>= 1
             else:
                 num -= 1
             iter +=1
