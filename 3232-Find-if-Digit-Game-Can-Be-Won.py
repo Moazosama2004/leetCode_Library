ac735511026694fdb5e322fc143f01b61828ad83
Moaz osama <102158567+Moazosama2004@users.noreply.github.com>
2024-08-02 14:33:34 +0300

    [3232] [Time Beats: 20.25%] [Memory Beats: 25.54%] - LeetPush

---
+++ 3232-Find-if-Digit-Game-Can-Be-Won.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canAliceWin(self, nums: List[int]) -> bool:
+        sum_double_digit = 0
+        sum_single_digit = 0
+
+        for num in nums:
+            if num > 9:
+                sum_double_digit += num
+            else:
+                sum_single_digit += num
+        return True if abs(sum_single_digit - sum_double_digit) != 0 else False
+
+            
